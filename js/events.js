f82b63ee9c9367826a7ec56148676e767f5df21c
Ronil Bhatia <ronilbhatia@gmail.com>
2017-10-15 00:02:41 +0000

    Done.

--- js/events.js
+++ js/events.js
@@ -1,7 +1,32 @@
 //define functions here
+function getIt() {
+  $('p').on('click', function (){
+    alert('Hey')
+  })
+}
 
+function frameIt() {
+  $('img').on('load',function() {
+    $('img').addClass('tasty')
+  })
+}
+
+function pressIt() {
+  $('input').on('keydown',function(e) {
+    if(e.which == 71) {
+      alert('I was clicked')
+    }
+  })
+}
+function submitIt() {
+$('form').on('submit', function() {
+  alert('Your form is going to be submitted now.')
+})
+}
 $(document).ready(function(){
-
 // call functions here
-
+getIt()
+frameIt()
+pressIt()
+submitIt()
 });
